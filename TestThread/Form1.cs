84f7c612a60cc9aca2c5fcab71ff0cdd8162d5fe
XiaoWei <40625200+luochen210@users.noreply.github.com>
2018-12-28 13:58:34 +0800

    添加项目文件。

---
+++ TestThread/Form1.cs
@@ -0,0 +1,63 @@
+﻿/// <summary>
+/// 编 码 人：苏飞
+/// 联系方式：361983679  
+/// 官方网址：http://www.sufeinet.com
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace TestThread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //创建一个委托，是为访问TextBox控件服务的。
+        public delegate void UpdateTxt(string msg);
+        //定义一个委托变量
+        public UpdateTxt updateTxt;
+
+        //修改TextBox值的方法。
+        public void UpdateTxtMethod(string msg)
+        {
+            richTextBox1.AppendText(msg + "\r\n");
+            richTextBox1.ScrollToCaret();
+        }
+
+        //此为在非创建线程中的调用方法，其实是使用TextBox的Invoke方法。
+        public void ThreadMethodTxt(int n)
+        {
+            this.BeginInvoke(updateTxt, "线程开始执行，执行" + n + "次，每一秒执行一次");
+            for (int i = 0; i < n; i++)
+            {
+                this.BeginInvoke(updateTxt, i.ToString());
+                //一秒 执行一次
+                Thread.Sleep(1000);
+            }
+            this.BeginInvoke(updateTxt, "线程结束");
+        }
+        //开启线程
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread objThread = new Thread(new ThreadStart(delegate
+            {
+                ThreadMethodTxt(Convert.ToInt32(textBox1.Text.Trim()));
+            }));
+            objThread.Start();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            //实例化委托
+            updateTxt = new UpdateTxt(UpdateTxtMethod);
+        }
+    }
+}
